23a8c63ca8d16ef279d6e9903e3ca21b3359e4ef
Brad Evans <bradleytuckerevans@gmail.com>
2023-04-25 13:37:50 -0400

    testing for HW3

---
+++ Address.java
@@ -0,0 +1,17 @@
+public abstract class Address{
+    protected String streetAddress;
+    protected String cityState;
+    protected int zipCode;
+
+
+
+    public Address(String streetAddress, String cityState, int zipCode) {
+        this.streetAddress = streetAddress;
+        this.cityState = cityState;
+        this.zipCode = zipCode;
+    }
+
+
+
+
+}
