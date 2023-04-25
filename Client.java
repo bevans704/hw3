fdc9df6a279d2c8d8a3d45ada58ca9576775d1cc
Brad Evans <bradleytuckerevans@gmail.com>
2023-04-25 13:33:16 -0400

    testing for HW3

---
+++ Client.java
@@ -0,0 +1,9 @@
+public class Client extends Person{
+    private int clientID;
+    private String clientStatus;
+    public Client(int clientID, String clientStatus){
+        this.clientID = clientID;
+        this.clientStatus = clientStatus;
+
+    }
+
