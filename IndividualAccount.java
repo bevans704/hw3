d014bfd67061519a274b11bfd970af6733c6980b
Brad Evans <bradleytuckerevans@gmail.com>
2023-04-25 13:19:29 -0400

    testing for HW3

---
+++ IndividualAccount.java
@@ -0,0 +1,8 @@
+public class IndividualAccount extends Account{
+    private boolean jointAccount;
+    private String memberLevel;
+    public IndividualAccount(boolean jointAccount, String memberLevel){
+        this.jointAccount = jointAccount;
+        this.memberLevel = memberLevel;
+
+    }
