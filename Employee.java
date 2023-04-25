1fcff2ea266a738e40f268e100c4990f3816d050
Brad Evans <bradleytuckerevans@gmail.com>
2023-04-25 13:30:24 -0400

    testing for HW3

---
+++ Employee.java
@@ -0,0 +1,9 @@
+public class Employee extends Person{
+    private int employeeID;
+    private String department;
+    public Employee(int employeeID, String department){
+        this.employeeID = employeeID;
+        this.department = department;
+
+    }
+
