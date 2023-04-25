111df20a562697395cacc9dfd72db408d30577f6
Brad Evans <bradleytuckerevans@gmail.com>
2023-04-25 13:43:01 -0400

    testing for HW3

---
+++ Transaction.java
@@ -0,0 +1,26 @@
+public abstract class Transaction{
+    protected String accountFrom;
+    protected String accountTo;
+    protected double amount;
+    protected String date;
+    protected String transactionType;
+    protected boolean domestic;
+    protected int refNumber;
+    protected String address
+
+
+    public Bank(String accountFrom, String accountTo, double amount, String date, String transactionType, boolean domestic, int refNumber String address) {
+        this.accountFrom = accountFrom;
+        this.accountTo = accountTo;
+        this.amount = amount;
+        this.date = date;
+        this.transactionType = transactionType;
+        this.domestic = domestic;
+        this.refNumber = refNumber;
+        this.address = address;
+    }
+
+
+
+
+}
