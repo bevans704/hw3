1ce4f8a0f86b698bd3e4bca15497f07ef3cb9aba
Brad Evans <bradleytuckerevans@gmail.com>
2023-04-25 13:27:14 -0400

    testing for HW3

---
+++ Person.java
@@ -0,0 +1,21 @@
+public abstract class Person{
+    protected String firstName;
+    protected String lastName;
+    protected String phoneNumber;
+    protected String email;
+    protected String address;
+
+
+    public Bank(String firstName, String lastName, String phoneNumber, String email, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+
+
+
+
+}
+
