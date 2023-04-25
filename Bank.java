fa27635c2c2e7d3d99228d2fffb85ebf6284e077
Brad Evans <bradleytuckerevans@gmail.com>
2023-04-25 09:42:07 -0400

    testing for HW3

--- Bank.java
+++ Bank.java
@@ -1,2 +1,22 @@
-public class Bank {
+public abstract class Bank{
+    protected String bankID;
+    protected String bankName;
+
+    protected String address;
+
+
+    public Bank(String bankID, String bankName, String address) {
+        this.bankID = bankID;
+        this.bankName = bankName;
+        this.address = address;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return ("bankID: " + bankID +
+                ", bankName: " + bankName +
+                ", address: " + address);
+    }
 }
