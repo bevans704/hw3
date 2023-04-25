51628c1a3284e96d86c30861eaca10cccff5d5ae
Brad Evans <bradleytuckerevans@gmail.com>
2023-04-25 13:22:41 -0400

    testing for HW3

---
+++ BusinessAccount.java
@@ -0,0 +1,9 @@
+public class BusinessAccount extends Account{
+    private boolean draftProtection;
+    private String upgradeOffer;
+    public BusinessAccount(boolean draftProtection, String upgradeOffer){
+        this.draftProtection = draftProtection;
+        this.upgradeOffer = upgradeOffer;
+
+    }
+
