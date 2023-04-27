a89eed3dbc1a89f7d9eeb7c6a62b5b427ca32494
Brad Evans <bradleytuckerevans@gmail.com>
2023-04-27 11:01:57 -0400

    testing for HW3

--- Bank.java
+++ Bank.java
@@ -1,25 +1,77 @@
-public abstract class Bank{
-    protected String bankID;
-    protected String bankName;
+import java.util.ArrayList;
+import java.util.Objects;
+public abstract class Bank implements IBank{
+    private String bankID;
+    private String bankName;
+    private Address address;
+    private ArrayList<Account> accounts;
+    private ArrayList<Employee> employees;
+    private ArrayList<Client> clients;
+    private TransactionJournal transactionJournal;
 
-    protected String address;
-    protected ArrayList<Account>;
-    protected ArrayList<Employee>;
-    protected ArrayList<Client>;
-    protected ArrayList<TransactionJournal>;
 
 
-
-    public Bank(String bankID, String bankName, String address, String accounts, String employees, String clients, String transactionJournal) {
+    public Bank(String bankID, String bankName, Address address, ArrayList<Account>, ArrayList<Employee>, ArrayList<Client>, TransactionJournal transactionJournal) {
         this.bankID = bankID;
         this.bankName = bankName;
         this.address = address;
-        this.accounts = new ArrayList<Account>();
-        this.employees = new ArrayList<Employee>();
-        this.clients = new ArrayList<Client>();
+        this.accounts = accounts;
+        this.employees = employees;
+        this.clients = clients;
         this.transactionJournal = new ArrayList<TransactionJournal>();
     }
 
+    public TransactionJournal getTransactionJournal() {
+        return transactionJournal;
+    }
+
+    public void setTransactionJournal(TransactionJournal transactionJournal) {
+        this.transactionJournal = transactionJournal
+    }
+    public String getBankID() {
+        return bankID;
+    }
+
+    public void setBankID(String bankID) {
+        this.bankID = bankID;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address){
+        this.address = address;
+    }
+    public ArrayList<Account> getAccounts(){
+        return accounts
+    }
+
+    public void setAccounts(ArrayList<Account> accounts) {
+        this.accounts = accounts;
+    }
+    public ArrayList<Employee> getEmployeess(){
+        return employees;
+    }
+
+    public void setEmployees(ArrayList<Employee> employees) {
+        this.employees = employees;
+    }
+    public ArrayList<Client> getClients(){
+        return clients
+    }
+
+    public void setClients(ArrayList<Client> clients) {
+        this.clients = clients;
+    }
+
+}
 
 
     @Override
--- Person.java
+++ Person.java
@@ -1,12 +1,15 @@
+import java.util.Objects;
+
+
 public abstract class Person{
     protected String firstName;
     protected String lastName;
     protected String phoneNumber;
     protected String email;
-    protected String address;
+    protected Address address;
 
 
-    public Bank(String firstName, String lastName, String phoneNumber, String email, String address) {
+    public Bank(String firstName, String lastName, String phoneNumber, String email, Address address) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
@@ -14,8 +17,44 @@ public abstract class Person{
         this.address = address;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
 
